a9c547a5495a480f4b7d64006fa19392cea13d0d
adrianezd <adrianezquerrods@gmail.com>
2022-10-18 19:27:55 +0200

    ConversorMonedas Beta 4 monedas y foto

--- ConversorDivisas1_Adrian/Form1.Designer.cs
+++ ConversorDivisas1_Adrian/Form1.Designer.cs
@@ -34,12 +34,21 @@
             this.tbEuros = new System.Windows.Forms.TextBox();
             this.lbResultadoPeseta = new System.Windows.Forms.Label();
             this.lbResultadoEuro = new System.Windows.Forms.Label();
+            this.lbResultadoDolares = new System.Windows.Forms.Label();
+            this.tbDolares = new System.Windows.Forms.TextBox();
+            this.lbDolares = new System.Windows.Forms.Label();
+            this.lbResultadoPesos = new System.Windows.Forms.Label();
+            this.tbPesos = new System.Windows.Forms.TextBox();
+            this.lbPesos = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lbPesetas
             // 
             this.lbPesetas.AutoSize = true;
-            this.lbPesetas.Location = new System.Drawing.Point(179, 97);
+            this.lbPesetas.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbPesetas.Location = new System.Drawing.Point(556, 79);
             this.lbPesetas.Name = "lbPesetas";
             this.lbPesetas.Size = new System.Drawing.Size(57, 16);
             this.lbPesetas.TabIndex = 0;
@@ -48,7 +57,8 @@
             // lbEuros
             // 
             this.lbEuros.AutoSize = true;
-            this.lbEuros.Location = new System.Drawing.Point(479, 97);
+            this.lbEuros.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbEuros.Location = new System.Drawing.Point(719, 79);
             this.lbEuros.Name = "lbEuros";
             this.lbEuros.Size = new System.Drawing.Size(42, 16);
             this.lbEuros.TabIndex = 1;
@@ -56,7 +66,7 @@
             // 
             // tbPesetas
             // 
-            this.tbPesetas.Location = new System.Drawing.Point(155, 147);
+            this.tbPesetas.Location = new System.Drawing.Point(534, 129);
             this.tbPesetas.Name = "tbPesetas";
             this.tbPesetas.Size = new System.Drawing.Size(100, 22);
             this.tbPesetas.TabIndex = 2;
@@ -64,15 +74,16 @@
             // 
             // tbEuros
             // 
-            this.tbEuros.Location = new System.Drawing.Point(452, 147);
+            this.tbEuros.Location = new System.Drawing.Point(691, 129);
             this.tbEuros.Name = "tbEuros";
             this.tbEuros.Size = new System.Drawing.Size(100, 22);
             this.tbEuros.TabIndex = 3;
+            this.tbEuros.TextChanged += new System.EventHandler(this.tbEuros_TextChanged_1);
             // 
             // lbResultadoPeseta
             // 
             this.lbResultadoPeseta.AutoSize = true;
-            this.lbResultadoPeseta.Location = new System.Drawing.Point(201, 211);
+            this.lbResultadoPeseta.Location = new System.Drawing.Point(540, 193);
             this.lbResultadoPeseta.Name = "lbResultadoPeseta";
             this.lbResultadoPeseta.Size = new System.Drawing.Size(14, 16);
             this.lbResultadoPeseta.TabIndex = 4;
@@ -81,17 +92,87 @@
             // lbResultadoEuro
             // 
             this.lbResultadoEuro.AutoSize = true;
-            this.lbResultadoEuro.Location = new System.Drawing.Point(495, 211);
+            this.lbResultadoEuro.Location = new System.Drawing.Point(699, 193);
             this.lbResultadoEuro.Name = "lbResultadoEuro";
             this.lbResultadoEuro.Size = new System.Drawing.Size(14, 16);
             this.lbResultadoEuro.TabIndex = 5;
             this.lbResultadoEuro.Text = "0";
             // 
+            // lbResultadoDolares
+            // 
+            this.lbResultadoDolares.AutoSize = true;
+            this.lbResultadoDolares.Location = new System.Drawing.Point(540, 393);
+            this.lbResultadoDolares.Name = "lbResultadoDolares";
+            this.lbResultadoDolares.Size = new System.Drawing.Size(14, 16);
+            this.lbResultadoDolares.TabIndex = 8;
+            this.lbResultadoDolares.Text = "0";
+            // 
+            // tbDolares
+            // 
+            this.tbDolares.Location = new System.Drawing.Point(534, 329);
+            this.tbDolares.Name = "tbDolares";
+            this.tbDolares.Size = new System.Drawing.Size(100, 22);
+            this.tbDolares.TabIndex = 7;
+            this.tbDolares.TextChanged += new System.EventHandler(this.tbDolares_TextChanged_1);
+            // 
+            // lbDolares
+            // 
+            this.lbDolares.AutoSize = true;
+            this.lbDolares.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbDolares.Location = new System.Drawing.Point(558, 279);
+            this.lbDolares.Name = "lbDolares";
+            this.lbDolares.Size = new System.Drawing.Size(55, 16);
+            this.lbDolares.TabIndex = 6;
+            this.lbDolares.Text = "Dolares";
+            // 
+            // lbResultadoPesos
+            // 
+            this.lbResultadoPesos.AutoSize = true;
+            this.lbResultadoPesos.Location = new System.Drawing.Point(701, 393);
+            this.lbResultadoPesos.Name = "lbResultadoPesos";
+            this.lbResultadoPesos.Size = new System.Drawing.Size(14, 16);
+            this.lbResultadoPesos.TabIndex = 11;
+            this.lbResultadoPesos.Text = "0";
+            // 
+            // tbPesos
+            // 
+            this.tbPesos.Location = new System.Drawing.Point(694, 329);
+            this.tbPesos.Name = "tbPesos";
+            this.tbPesos.Size = new System.Drawing.Size(100, 22);
+            this.tbPesos.TabIndex = 10;
+            this.tbPesos.TextChanged += new System.EventHandler(this.tbPesos_TextChanged_1);
+            // 
+            // lbPesos
+            // 
+            this.lbPesos.AutoSize = true;
+            this.lbPesos.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbPesos.Location = new System.Drawing.Point(691, 279);
+            this.lbPesos.Name = "lbPesos";
+            this.lbPesos.Size = new System.Drawing.Size(114, 16);
+            this.lbPesos.TabIndex = 9;
+            this.lbPesos.Text = "Pesos Mexicanos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ConversorDivisas1_Adrian.Properties.Resources.free_dollar_coin_icon_2149_thumb1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(458, 473);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(871, 505);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbResultadoPesos);
+            this.Controls.Add(this.tbPesos);
+            this.Controls.Add(this.lbPesos);
+            this.Controls.Add(this.lbResultadoDolares);
+            this.Controls.Add(this.tbDolares);
+            this.Controls.Add(this.lbDolares);
             this.Controls.Add(this.lbResultadoEuro);
             this.Controls.Add(this.lbResultadoPeseta);
             this.Controls.Add(this.tbEuros);
@@ -100,6 +181,7 @@
             this.Controls.Add(this.lbPesetas);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,6 +195,13 @@
         private System.Windows.Forms.TextBox tbEuros;
         private System.Windows.Forms.Label lbResultadoPeseta;
         private System.Windows.Forms.Label lbResultadoEuro;
+        private System.Windows.Forms.Label lbResultadoDolares;
+        private System.Windows.Forms.TextBox tbDolares;
+        private System.Windows.Forms.Label lbDolares;
+        private System.Windows.Forms.Label lbResultadoPesos;
+        private System.Windows.Forms.TextBox tbPesos;
+        private System.Windows.Forms.Label lbPesos;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- ConversorDivisas1_Adrian/Form1.cs
+++ ConversorDivisas1_Adrian/Form1.cs
@@ -17,17 +17,157 @@ namespace ConversorDivisas1_Adrian
             InitializeComponent();
         }
 
-        private void tbPesetas_TextChanged(object sender, EventArgs e)
+        private void Conversor(double cantidad, string moneda)
         {
-            if (!string.IsNullOrEmpty(tbPesetas.Text)){
-                double pesetas = Convert.ToDouble((tbPesetas.Text));
+            if(moneda == "Euros")
+            {
+                double euros = Convert.ToDouble(cantidad);
+                lbResultadoDolares.Text = (Convert.ToDouble(euros) * 0.99).ToString();
+                lbResultadoPeseta.Text = (Convert.ToDouble(euros) * 166.386).ToString();
+                lbResultadoPesos.Text = (Convert.ToDouble(euros) * 19.75).ToString();
+            }
+            if (moneda == "Dolares")
+            {
+                double dolares = Convert.ToDouble(cantidad);
+                lbResultadoEuro.Text = (Convert.ToDouble(dolares) * 1.01).ToString();
+                lbResultadoPeseta.Text = (Convert.ToDouble(dolares) * 171.122).ToString();
+                lbResultadoPesos.Text = (Convert.ToDouble(dolares) * 20.03).ToString();
+            }
+            if (moneda == "Pesos")
+            {
+                double pesos = Convert.ToDouble(cantidad);
+                lbResultadoDolares.Text = (Convert.ToDouble(pesos) * 0.050).ToString();
+                lbResultadoEuro.Text = (Convert.ToDouble(pesos) * 0.051).ToString();
+                lbResultadoPeseta.Text = (Convert.ToDouble(pesos) * 8.52632).ToString();
+            }
+            if (moneda == "Pesetas")
+            {
+                double pesetas = Convert.ToDouble(cantidad);
+                lbResultadoDolares.Text = (Convert.ToDouble(pesetas) * 0.00589336).ToString();
                 pesetas = pesetas / 166.386;
                 lbResultadoEuro.Text = pesetas.ToString();
+                lbResultadoPesos.Text = (Convert.ToDouble(lbResultadoDolares.Text) * 20.04).ToString();
+            }
+        }
+
+        private void tbPesetas_TextChanged(object sender, EventArgs e)
+        {
+            tbEuros.Text = ""; tbDolares.Text = ""; tbPesos.Text = "";
+            if (!string.IsNullOrEmpty(tbPesetas.Text)){
+                if (tbPesetas.Text.All(char.IsDigit) || (!tbPesetas.Text.StartsWith(",")))
+                {
+                    
+                    double valor = Convert.ToDouble(tbPesetas.Text);
+                    Conversor(valor,"Pesetas");
+                }
+                else
+                {
+                    tbPesetas.Text = tbPesetas.Text.Remove((tbPesetas.Text).Length - 1);
+                }
             }
 
             if (!string.IsNullOrEmpty(tbPesetas.Text))
             {
                 lbResultadoPeseta.Text = tbPesetas.Text;
+            }
+
+        }
+        private void tbPesos_TextChanged(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void tbDolares_TextChanged(object sender, EventArgs e)
+        {
+            tbEuros.Text = ""; tbPesetas.Text = ""; tbPesos.Text = "";
+            if (!string.IsNullOrEmpty(tbDolares.Text))
+            {
+                if (tbDolares.Text.All(char.IsDigit) || (!tbDolares.Text.StartsWith(",")))
+                {
+                    double valor = Convert.ToDouble(tbDolares.Text);
+                    Conversor(valor, "Dolares");
+                }
+                else
+                {
+                    tbDolares.Text = tbDolares.Text.Remove((tbDolares.Text).Length - 1);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(tbDolares.Text))
+            {
+                lbResultadoDolares.Text = tbDolares.Text;
+            }
+
+        }
+
+        private void tbEuros_TextChanged(object sender, EventArgs e) { }
+       
+
+        private void tbEuros_TextChanged_1(object sender, EventArgs e)
+        {
+            tbPesetas.Text = ""; tbDolares.Text = ""; tbPesos.Text = "";
+            if (!string.IsNullOrEmpty(tbEuros.Text))
+            {
+                if (tbEuros.Text.All(char.IsDigit) || (!tbEuros.Text.StartsWith(",")))
+                {
+                    double valor = Convert.ToDouble(tbEuros.Text);
+                    Conversor(valor, "Euros");
+                }
+                else
+                {
+                    tbEuros.Text = tbEuros.Text.Remove((tbEuros.Text).Length - 1);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(tbEuros.Text))
+            {
+                lbResultadoEuro.Text = tbEuros.Text;
+            }
+
+        }
+
+        private void tbDolares_TextChanged_1(object sender, EventArgs e)
+        {
+            tbEuros.Text = ""; tbPesetas.Text = ""; tbPesos.Text = "";
+            if (!string.IsNullOrEmpty(tbDolares.Text))
+            {
+                if (tbDolares.Text.All(char.IsDigit) || (!tbDolares.Text.StartsWith(",")))
+                {
+                    double valor = Convert.ToDouble(tbDolares.Text);
+                    Conversor(valor, "Dolares");
+                }
+                else
+                {
+                    tbDolares.Text = tbDolares.Text.Remove((tbDolares.Text).Length - 1);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(tbDolares.Text))
+            {
+                lbResultadoDolares.Text = tbDolares.Text;
+            }
+
+        }
+
+        private void tbPesos_TextChanged_1(object sender, EventArgs e)
+        {
+            tbEuros.Text = ""; tbDolares.Text = ""; tbPesetas.Text = "";
+            if (!string.IsNullOrEmpty(tbPesos.Text))
+            {
+                if (tbPesos.Text.All(char.IsDigit) || (!tbPesos.Text.StartsWith(",")))
+                {
+                    double valor = Convert.ToDouble(tbPesos.Text);
+                    Conversor(valor, "Pesos");
+                }
+                else
+                {
+                    tbPesos.Text = tbPesos.Text.Remove((tbPesos.Text).Length - 1);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(tbPesos.Text))
+            {
+                lbResultadoPesos.Text = tbPesos.Text;
             }
 
         }
